0526acb582dde1d7876105373a40defbd60b0eac
Eirik Klebo <eiriktobias@gmail.com>
2023-06-11 10:41:05 +0200

    tailwinded login page

--- public/js/script.js
+++ public/js/script.js
@@ -2,3 +2,11 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("ace-of-fades JS imported successfully!");
 });
+
+/////////////// NAVBAR ///////////////
+const navLinks = document.querySelector(".nav-links");
+function onToggleMenu(e) {
+  e.name = e.name === "menu" ? "close" : "menu";
+  navLinks.classList.toggle("top-[9%]");
+}
+//////////////////////////////////////
