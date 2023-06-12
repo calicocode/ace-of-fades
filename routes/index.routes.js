2a5ab84b6739bbfc595600964064e814833868ec
Eirik Klebo <eiriktobias@gmail.com>
2023-06-12 10:50:02 +0200

    created contact page

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,9 +1,19 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
+});
+
+/* GET contact page */
+router.get("/contact", (req, res, next) => {
+  res.render("contact");
+});
+
+/* GET about page */
+router.get("/about", (req, res, next) => {
+  res.render("about");
 });
 
 module.exports = router;
