4bd3927705a68fc7a5e28cdf08724223fdbf3e34
Jani Ojala <jani@stptrade.com>
2023-06-14 20:55:29 +0200

    working on user upload style

--- routes/customer.routes.js
+++ routes/customer.routes.js
@@ -7,6 +7,7 @@ const isLoggedIn = require("../middleware/isLoggedIn.js");
 const isAdmin = require("../middleware/isAdmin.js");
 
 const fileUploader = require("../config/cloudinary.config");
+const req = require("express/lib/request");
 
 router.get("/my-styles", isLoggedIn, (req, res, next) => {
   User.findById(req.session.currentUser._id)
@@ -30,33 +31,37 @@ router.post(
   fileUploader.single("imageURL"),
   isLoggedIn,
   (req, res, next) => {
-    const newStyle = {
-      myUploadedStyles: {
-        title: req.body.title,
-        slogan: req.body.slogan,
-        imageURL: req.file.path,
+    User.findByIdAndUpdate(req.session.currentUser._id, {
+      $set: {
+        myUploadedStyles: {
+          title: req.body.title,
+          slogan: req.body.slogan,
+          imageURL: req.file.path,
+        },
       },
-    };
-
-    User.findByIdAndUpdate(req.session.currentUser._id, newStyle).then(
-      (newStyle) => {
+    });
+    res.redirect("/my-styles").catch((e) => {
+      console.log("error getting list of users styles from DB", e);
+      next(e);
+    });
+  }
+);
+/*       .then((newStyle) => {
         res.redirect("/my-styles");
-      }
-    );
-
-    User.find()
-      .then((myStylesFromDB) => {
-        console.log(myStylesFromDB);
-        const data = {
-          myStyles: myStylesFromDB,
-        };
-        res.render("customer/my-styles", data);
       })
-      .catch((e) => {
-        console.log("error getting list of users styles from DB", e);
+      .catch((e) => { */
+
+/*         },
+      },
+    })
+      .then((newStyle) => {
+        res.redirect("/my-styles");
+      })
+      .catch((e) => { */
+/*         console.log("error getting list of users styles from DB", e);
         next(e);
       });
   }
-);
+); */
 
 module.exports = router;
